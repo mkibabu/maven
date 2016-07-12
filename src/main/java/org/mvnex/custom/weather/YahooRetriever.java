1b8b3519a8b1efbf99fad13d5281180e679a12c0
Michael Muraya <mkibabu@gmail.com>
2016-07-11 22:58:11 -0400

    Change Yahoo url structure to match current version

--- src/main/java/org/mvnex/custom/weather/YahooRetriever.java
+++ src/main/java/org/mvnex/custom/weather/YahooRetriever.java
@@ -3,6 +3,7 @@ package org.mvnex.custom.weather;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import org.apache.log4j.Logger;
 
@@ -12,8 +13,10 @@ public class YahooRetriever {
 
     public InputStream retrieve(String zipcode) throws Exception {
         log.info( "Retrieving Weather Data" );
-        String url = "http://weather.yahooapis.com/forecastrss?p="
-          + zipcode;
+        String query = "select * from weather.forecast where woeid in "+ 
+        "(select woeid from geo.places(1) where text=\"" + zipcode + "\")";
+        String url = "https://query.yahooapis.com/v1/public/yql?q="
+          + URLEncoder.encode(query, "UTF-8");
         URLConnection conn = new URL(url).openConnection();
         return conn.getInputStream();
     }
